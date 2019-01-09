6402560a9ef279cca5bfe220969206c83b301681
Arun Singh <arunsin997@gmail.com>
2019-01-09 22:36:20 +0530

    first assignment completed

---
+++ Assignment 1/Ass1_52_1b.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  int i;
+  char cmd[100];
+  // if(argc >= 2){
+  //   printf("The arguments supplied are : \n");
+  //   for(int = i; i<argc; i++)
+  //     printf("%s\t", argc[i]);
+  // }
+  while(1){
+    scanf(" %[^\n]%*c",cmd);
+    // if(argc == 1){
+    //   printf("NO ARGUMENTS DETECTED\n");
+    // } else  {
+          if(strcmp(cmd, "quit") == 0)
+            break;
+    // }
+    // printing the command line arguments
+    for(int i=0; i<argc; i++){
+      printf("%s\n", argv[i]);
+    }
+
+    // CLONING THE PROCESS TO RUN THE COMMAND ENTERED BY THE USER
+    if(!fork()){    // CHILD PROCESS
+      if(execlp(cmd, cmd , NULL) == -1)
+        perror("exec");
+    }
+  }
+  return 0;
+}
